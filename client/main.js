b8d00f598e2ed40691c8b21fa226606695a1710b
Jeremy Koloski <jeremy.koloski@comcast.net>
2016-05-12 02:24:35 -0400

    image gallery!!

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './covers.html';
 
 Router.route('/photoshop');
 Router.route('/originals');
@@ -13,5 +14,25 @@ Router.route('/', {
 });
 Router.configure({
     layoutTemplate: 'main'
+});
+
+Template.covers.events({
+  'click .thumbnail': function(){   
+		var image = document.getElementById('galleryImage');
+		if(event.target.id=="venice"){
+			//$("body").addClass("gray");
+			$(".gray").show();
+		}
+		if (event.target.id=="venice") {
+        image.src = "/images/venice.jpg";
+		} else {
+        image.src = "/images/sherlock.jpg";
+		}
+		
+    },
+	'click #remove': function(){ 
+		console.log("clicked");
+		$(".gray").hide();
+	}	
 });
 
